46c3e6033ee6e61d1f85d77f9b9d7e9caaca831e
Jonathan van Wersch <jonathanvwersch@gmail.com>
2020-12-21 18:52:17 +0100

    test commit

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import * as Icons from "react-icons/md";
 import DropBlock from "./DropBlock";
 import "./Sidebar.css";
